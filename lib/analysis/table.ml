cc67bff03629906335bc3d79a6f209cbdc0883af
Jacob Rosales Chase <rosaleschase.j@gmail.com>
2020-11-14 11:35:05 -0500

    Update the way symbols are entered into the table

--- lib/analysis/table.ml
+++ lib/analysis/table.ml
@@ -4,9 +4,6 @@ type 'a t = (Symbol.t, 'a, Symbol.comparator_witness) Map.t
 
 let empty = Map.empty (module Symbol)
 
-let enter table symbol x =
-  match Map.add table ~key:symbol ~data:x with
-  | `Ok table -> table
-  | `Duplicate -> assert false
+let enter table symbol x = Map.set table ~key:symbol ~data:x
 
 let look table symbol = Map.find table symbol
